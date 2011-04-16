df7d48f35b91c915846b46d67a35d8fb10271eb1
Uli Kusterer <ulivc@zathras.de>
2011-04-16 16:37:06 +0200

    Forge/Leonie: Make global properties customizable.

---
+++ generic/LEOGlobalProperties.c
@@ -0,0 +1,13 @@
+//
+//  LEOGlobalProperties.c
+//  Leonie
+//
+//  Created by Uli Kusterer on 10.04.11.
+//  Copyright 2011 The Void Software. All rights reserved.
+//
+
+#include "LEOGlobalProperties.h"
+
+
+size_t			kFirstGlobalPropertyInstruction = 0;
+
