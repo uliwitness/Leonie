2858c95eea3233fddf6d561c4e1b30f1d11916cb
Uli Kusterer <ulivc@zathras.de>
2014-02-27 23:53:27 +0100

    Add UTF8StringParseUTF32CharacterAtOffset to header.

--- generic/UTF8UTF32Utilities.h
+++ generic/UTF8UTF32Utilities.h
@@ -16,12 +16,14 @@
 #if __cplusplus
 extern "C" {
 #endif
+	
+	uint32_t	UTF32CharacterToLower( uint32_t inUTF32Char );
 
 	size_t		GetLengthOfUTF8SequenceStartingWith( unsigned char inChar );
 	uint32_t	UTF8StringParseUTF32CharacterAtOffset( const char *utf8, size_t len, size_t *ioOffset );
 	void		UTF8BytesForUTF32Character( uint32_t utf32Char, char* utf8, size_t *outLength );
+	uint32_t	UTF8StringParseUTF32CharacterAtOffset( const char *utf8, size_t len, size_t *ioOffset );
 	size_t		UTF8LengthForUTF32Char( uint32_t utf32Char );
-	uint32_t	UTF32CharacterToLower( uint32_t inUTF32Char );
 
 	uint32_t	UTF16StringParseUTF32CharacterAtOffset( const uint16_t *utf16, size_t bytesLen, size_t *ioCharOffset );
 	size_t		UTF16LengthForUTF32Char( uint32_t inChar );
