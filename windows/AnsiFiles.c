3815e9b32c4868ae5c925ed5d13a91573661909f
Uli Kusterer <ulivc@zathras.de>
2019-03-01 21:55:54 +0100

    Windows has _getcwd() that we can use.

--- windows/AnsiFiles.c
+++ windows/AnsiFiles.c
@@ -2,13 +2,6 @@
 #include <assert.h>
 #include <stddef.h>
 
-const char* getcwd(char* buf, size_t bufSize)
-{
-	assert(bufSize > 0);
-	buf[0] = 0;
-	return buf;
-}
-
 FILE* LEOFOpen(const char* path, const char* mode)
 {
 	FILE* theFile = NULL;
--- windows/AnsiFiles.h
+++ windows/AnsiFiles.h
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <direct.h>
 
 #if __cplusplus
 #include <filesystem>
@@ -10,7 +11,7 @@ using namespace std;
 extern "C" {
 #endif
 
-const char* getcwd(char* buf, size_t bufSize);
+#define getcwd _getcwd
 FILE* LEOFOpen(const char* path, const char* mode);
 
 #if __cplusplus
